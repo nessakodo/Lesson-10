acac985128e5b8275f43c2a7506942258f7dbd47
nessa kodo <nessakodo@gmail.com>
2022-11-08 17:31:12 -0700

    some errors will fix

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -3,11 +3,23 @@ pragma solidity ^0.8.17;
 
 // import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+interface IMyERC20 {
+    function mint(address to, uint256 amount) external;
+}
+
 contract TokenSale {
     uint256 public ratio;
+    address public paymentToken;
     // underscore helps us make a state variable
-    constructor(uint256 _ratio) {
+    constructor(uint256 _ratio, address _paymentToken) {
         ratio = _ratio;
+        paymentToken = IMyERC20(_paymentToken);
 
+    }
+
+    function buyTokens() external payable {
+        uint256 amountToBeMinted = msg.value / ratio;
+        paymentToken.mint(msg.sender, amount);
+        
     }
 }
--- tests/Test.ts
+++ tests/Test.ts
@@ -1,35 +1,77 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { TokenSale } from "../typechain-types";
-import { ERC20 } from "../typechain-types";
-import { ERC721 } from "../typechain-types";
+import {
+  MyERC20,
+  MyERC20__factory,
+  MyERC721__factory,
+  TokenSale,
+  TokenSale__factory,
+} from "../typechain-types";
+
+
 
 const TEST_RATIO = 1;
 
 describe("NFT Shop", async () => {
-  let tokenSalecontract: TokenSale;
+  let tokenSaleContract: TokenSale;
+  let paymentTokenContract: MyERC20;
+  let erc20ContractFactory: MyERC20__factory;
+  let erc721ContractFactory: MyERC721__factory;
+  let tokenSaleContractFactory: TokenSale__factory;
+
 
   beforeEach(async () => {
-    const tokenSaleContractFactory = await ethers.getContractFactory(
-      "TokenSale"
+    const [
+      accounts,
+      erc20ContractFactory,
+      erc721ContractFactory,
+      tokenSaleConractFactory,
+    ] = await Promise.all([
+      ethers.getSigners(),
+      ethers.getContractFactory("MyERC20"),
+      ethers.getContractFactory("MyERC721"),
+      ethers.getContractFactory("TokenSale"),
+    ]);
+    paymentTokenContract = await erc20ContractFactory.deploy();
+    await paymentTokenContract.deployed();
+    tokenSaleContract = await tokenSaleFactory.deploy(
+      TEST_RATIO, 
+      paymentTokenContract.address
       );
-    tokenSalecontract = await tokenSaleContractFactory.deploy(TEST_RATIO) as TokenSale;
-    await tokenSalecontract.deployed();
+    await tokenSaleContract.deployed();
+    const MINTER_ROLE = await paymentTokenContract.MINTER_ROLE();
+    const roleTx = await paymentTokenContract.grantRole(
+      MINTER_ROLE, 
+      paymentTokenContract.address
+    );
+    await roleTx.wait();
   });
 
   describe("When the Shop contract is deployed", async () => {
     it("defines the ratio as provided in parameters", async () => {
-      const ratio = await tokenSalecontract.ratio();
+      const ratio = await tokenSaleContract.ratio();
       expect(ratio).to.eq(TEST_RATIO);
     });
 
     it("uses a valid ERC20 as payment token", async () => {
-      throw new Error("Not implemented");
+      const paymentAddress =
+        await tokenSaleContract.paymentToken();
+      const paymentContract = erc20ContractFactory.attach(paymentAddress);
+      await expect(paymentContract.balanceOf(accounts[0].address)).not.to.be.reverted;
+      await expect(paymentContract.totalSupply()).not.to.be.reverted;
+
     });
   });
 
   describe("When a user purchase an ERC20 from the Token contract", async () => {
-    beforeEach(async () => {});
+    const buyValue = ethers.utils.parseEther("1");
+    beforeEach(async () => {
+    const tx = await tokenSaleContract
+    .connect(accounts[1])
+    .buyTokens({value: buyValue});
+  await tx.wait();
+  });
 
     it("charges the correct amount of ETH", async () => {
       throw new Error("Not implemented");
